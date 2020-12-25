8cd7dbbfd0579750669cb946f6576f5cce777b58
devebrahim <sadeghiebrahim.dev@gmail.com>
2020-12-25 06:17:04 +0330

    improve project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SnappLogin';
-}
+  password:string
+  username:string
+  constructor() { }
+  ngOnInit() {}
+  abb(karbari: Event){this.username=(<HTMLInputElement>karbari.target).value}}
